2e848602b3d551c9ece13d47d07bec8d3b12d0a7
Matthew Sato <91570784+satomm1@users.noreply.github.com>
2026-03-11 14:44:25 -0700

    Add trajectory smoothing function using cubic splines

--- utils.py
+++ utils.py
@@ -1,7 +1,53 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap, BoundaryNorm
+import scipy.interpolate
 
 def snap_to_grid(x, resolution):
     return (resolution * round(x[0] / resolution), resolution * round(x[1] / resolution))
 
+def wrapToPi(a):
+    if isinstance(a, list):
+        return [(x+np.pi) % (2*np.pi) - np.pi for x in a]
+    return (a + np.pi) % (2*np.pi) - np.pi
+
+def compute_smoothed_traj(path, V_des, k, alpha, dt):
+    """
+    Fit cubic spline to a path and generate a resulting trajectory for our
+    wheeled robot.
+
+    Inputs:
+        path (np.array [N,2]): Initial path
+        V_des (float): Desired nominal velocity, used as a heuristic to assign nominal
+            times to points in the initial path
+        k (int): The degree of the spline fit.
+            For this assignment, k should equal 3 (see documentation for
+            scipy.interpolate.splrep)
+        alpha (float): Smoothing parameter (see documentation for
+            scipy.interpolate.splrep)
+        dt (float): Timestep used in final smooth trajectory
+    Outputs:
+        t_smoothed (np.array [N]): Associated trajectory times
+        traj_smoothed (np.array [N,7]): Smoothed trajectory
+    Hint: Use splrep and splev from scipy.interpolate
+    """
+    assert(path and k > 2 and k < len(path))
+    ########## Code starts here ##########
+    t = np.zeros(len(path))
+    for ii in range(len(path) - 1):
+        dist = np.linalg.norm(np.array(path[ii + 1]) - np.array(path[ii]))  # distance between consecutive points
+        t[ii + 1] = dist / V_des + t[ii]  # time at next point assuming constant velocity V_des
+
+    tck_x = scipy.interpolate.splrep(t, np.array(path)[:, 0], k=k, s=alpha)
+    tck_y = scipy.interpolate.splrep(t, np.array(path)[:, 1], k=k, s=alpha)
+
+    t_smoothed = np.arange(0, t[-1], dt)
+    x_d = scipy.interpolate.splev(t_smoothed, tck_x, der=0)
+    y_d = scipy.interpolate.splev(t_smoothed, tck_y, der=0)
+    xd_d = scipy.interpolate.splev(t_smoothed, tck_x, der=1)
+    yd_d = scipy.interpolate.splev(t_smoothed, tck_y, der=1)
+    xdd_d = scipy.interpolate.splev(t_smoothed, tck_x, der=2)
+    ydd_d = scipy.interpolate.splev(t_smoothed, tck_y, der=2)
+    theta_d = np.arctan2(yd_d, xd_d)
+    ########## Code ends here ##########
+    traj_smoothed = np.stack([x_d, y_d, theta_d, xd_d, yd_d, xdd_d, ydd_d]).transpose()
+
+    return t_smoothed, traj_smoothed
