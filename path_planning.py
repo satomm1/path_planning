7ec0343ec34383bb688a7007815c3409dd34250d
Matthew Sato <91570784+satomm1@users.noreply.github.com>
2026-03-11 14:54:48 -0700

    Update occupancy grid generation and adjust initial/goal positions in path planning

--- path_planning.py
+++ path_planning.py
@@ -343,12 +343,14 @@ if __name__ == "__main__":
     map_resolution = 0.2
     map_dim = [round(map_size[i]/map_resolution) for i in range(len(map_size))]
 
-    occ = generate_sample_grid2(map_size, map_resolution, plot=False)
+    occ = generate_hall_with_room_grid(map_size, map_resolution, plot=False)
+
+    # occ = generate_sample_grid2(map_size, map_resolution, plot=False)
 
     occ_grid = StochOccupancyGrid2D(map_resolution, map_dim[0], map_dim[1], 0, 0, 10, occ.T)
 
-    x_init = snap_to_grid([90,3], map_resolution)
-    x_goal = snap_to_grid([90,97], map_resolution)
+    x_init = snap_to_grid([60, 45], map_resolution)
+    x_goal = snap_to_grid([15, 3], map_resolution)
     problem = AStar([0,0], snap_to_grid(map_size, map_resolution), x_init, x_goal, occ_grid, resolution=map_resolution)
 
     problem_status = problem.solve(plot=False)
